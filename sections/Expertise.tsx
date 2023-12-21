9765675f04d1a1f3c65a9e0c9ccf2350a09d2bdd
Ethan Flores <ethanf.flores@gmail.com>
2023-12-21 16:39:36 -0500

    expertise cards color and sizing

--- sections/Expertise.tsx
+++ sections/Expertise.tsx
@@ -3,14 +3,11 @@ import ExpertiseCard from "@/components/ExpertiseCard";
 
 const Expertise = () => {
   return (
-    <section
-      id="therapy"
-      className="max-container flex flex-col border-green-700 border-2"
-    >
-      <h1 className="flex justify-center text-[40px] font-bold pb-10">
+    <section id="therapy" className="max-container flex flex-col">
+      <h1 className="flex justify-center text-[40px] font-bold pb-10 text-stone-blue">
         Gen's Expertise
       </h1>
-      <div className="flex flex-col justify-center gap-[72px] flex-wrap md:flex-row border-green-300 border-2">
+      <div className="flex flex-col justify-center gap-[72px] flex-wrap md:flex-row">
         {specialties.map((specialty) => (
           <ExpertiseCard key={specialty.title} {...specialty} />
         ))}
