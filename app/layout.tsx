7d0d2ee70772faffec4c8bd3a4016b9fb8e989c9
radodd <ethanf.flores@gmail.com>
2024-01-19 13:15:28 -0800

    Vercel Analytics enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -14,7 +15,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <main className="relative overflow-hidden">{children}</main>
+        <main className="relative overflow-hidden">
+          {children}
+          <Analytics />
+        </main>
       </body>
     </html>
   );
