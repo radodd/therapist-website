2dc101e3593bc68aa9a0a8ed80d83c0c9cec5077
radodd <ethanf.flores@gmail.com>
2024-10-30 09:53:39 -0700

    fixed cv typo

--- sections/Intro.tsx
+++ sections/Intro.tsx
@@ -106,7 +106,7 @@ const Intro = () => {
             type="button"
             variant="btn-transparent"
             onClick={() =>
-              window.open("/docs/Gen Fulton's Curriculum Vitae OCt 2024.pdf")
+              window.open("/docs/Gen Fulton's Curriculum Vitae Oct 2024.pdf")
             }
           />
           <div className="flex justify-center items-center pt-6 mb-10">
