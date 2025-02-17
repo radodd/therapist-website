063cd88904015375b9161319e82d4d158572a6cb
radodd <ethanf.flores@gmail.com>
2025-02-17 11:05:51 -0500

    add custom email, updated privacy policy, refactor footer tsx

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -3,177 +3,122 @@ import Image from "next/image";
 import Link from "next/link";
 import { footerLinks } from "@/constants";
 
-import Button from "../components/Button";
+import styles from "../app/scss/Footer.module.scss";
 
 const Footer = () => {
   return (
-    <>
-      <footer className="flex justify-center w-full bg-primary px-[70px] py-6 max-mobileplus:px-3">
-        {/* condition render for normal screen size  */}
-        <div className="max-small:hidden flex justify-between text-white w-full ">
-          <div className="flex items-center gap-10">
-            <div className="flex items-center text-[40px] gap-4">
-              <Link href="/" className="">
-                <Image
-                  src="/favicon.ico"
-                  alt="Gen fulton website logo"
-                  width={70}
-                  height={70}
-                />
-              </Link>
-              <h2>Gen Fulton</h2>
-            </div>
-            <div className="grid grid-rows-3 grid-flow-col gap-1">
-              {footerLinks.map((item, index) => (
-                <ul key={item.name} className="">
-                  <li className={index > 2 ? "justify-self-center" : ""}>
-                    {index > 2 ? (
-                      <Link
-                        href={item.href}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="ml-10"
-                      >
-                        {item.name}
-                      </Link>
-                    ) : (
-                      <Link href={item.href}>{item.name}</Link>
-                    )}
-                  </li>
-                </ul>
-              ))}
-            </div>
-          </div>
-          <div className="text-white flex flex-col justify-center items-end gap-2">
-            <h3>Gen Fulton Consultancy</h3>
-
-            <Link href="tel:+14155792187">
-              <div className="flex gap-2 aspect-3/4">
-                <Image
-                  src="/footer/whatsapp.png"
-                  alt="Gen Fulton's WhatsApp phone number"
-                  width={24}
-                  height={24}
-                />
-                <span>+1 (415) 579-2187</span>
-              </div>
-            </Link>
-
-            <div className="flex gap-4">
-              <Link
-                href="https://www.linkedin.com/in/gen-fulton-lmft-sep-96a38566/"
-                target="_blank"
-              >
-                <Image
-                  src="/footer/linkedin.png"
-                  alt="Gen Fulton's LinkedIn"
-                  width={18}
-                  height={17}
-                />
-              </Link>
-              <Link
-                href="https://www.instagram.com/genfultonconsultancy/"
-                target="_blank"
-              >
-                <Image
-                  src="/footer/instagram.png"
-                  alt="Gen Fulton's Instagram"
-                  width={18}
-                  height={17}
-                />
-              </Link>
-            </div>
-          </div>
+    <footer className={styles.footerContainer}>
+      <FooterLinks />
+      <div className="text-white flex flex-col justify-center items-center gap-2">
+        <ContactInfo />
+        <SocialIcons />
+        <div className={styles.docContainer}>
+          <Link href="/docs/Privacy Policy.pdf" target="_blank">
+            <span>Privacy Policy | </span>
+          </Link>
+          <Link href="/docs/Disclaimer.pdf" target="_blank">
+            <span>Disclaimer</span>
+          </Link>
         </div>
-
-        {/* Conditional render for mobile screen  */}
-        <div className="small:hidden justify-center items-center place-content-center">
-          <div className="flex flex-col justify-center items-center">
-            <div className="flex  gap-6 flex-wrap text-white">
-              <Link href="/" className="flex items-center">
-                <Image
-                  src="/favicon.ico"
-                  alt="Gen Fulton website logo"
-                  width={70}
-                  height={70}
-                />
-              </Link>
-              <div className="grid grid-rows-3 grid-flow-col gap-1 pb-6">
-                {footerLinks.map((item, index) => (
-                  <ul key={item.name}>
-                    <li
-                      className={`${
-                        index <= footerLinks.length - 3 ? "" : "hidden"
-                      }`}
-                    >
-                      {index > 2 ? (
-                        <Link
-                          href={item.href}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          className="pl-10 max-mobileplus:pl-3"
-                        >
-                          {item.name}
-                        </Link>
-                      ) : (
-                        <Link href={item.href}>{item.name}</Link>
-                      )}
-                    </li>
-                  </ul>
-                ))}
-              </div>
-            </div>
-            <div className="text-white flex flex-col justify-center items-center gap-2">
-              <h3>Gen Fulton Consultancy</h3>
-              <Link href="tel:+14155792187">
-                <div className="flex gap-2 aspect-3/4">
-                  <Image
-                    src="/footer/whatsapp.png"
-                    alt="Gen Fulton's WhatsApp phone number"
-                    width={24}
-                    height={24}
-                  />
-                  <span>+1 (415) 579-2187</span>
-                </div>
-              </Link>
-              <div className="flex gap-4">
-                <Link
-                  href="https://www.linkedin.com/in/gen-fulton-lmft-sep-96a38566/"
-                  target="_blank"
-                >
-                  <Image
-                    src="/footer/linkedin.png"
-                    alt="Gen Fulton's LinkedIn"
-                    width={18}
-                    height={17}
-                  />
-                </Link>
-                <Link
-                  href="https://www.instagram.com/genfultonconsultancy/"
-                  target="_blank"
-                >
-                  <Image
-                    src="/footer/instagram.png"
-                    alt="Gen Fulton's Instagram"
-                    width={18}
-                    height={17}
-                  />
-                </Link>
-              </div>
-              <div>
-                <Link href="/docs/Privacy Policy.pdf" target="_blank">
-                  <span>Privacy Policy | </span>
-                </Link>
-                <Link href="/docs/Disclaimer.pdf" target="_blank">
-                  <span>Disclaimer</span>
-                </Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer>
-    </>
+      </div>
+    </footer>
   );
 };
+
+const FooterLinks = () => (
+  <div className={styles.navLinks}>
+    <Link href="/" className="flex items-center">
+      <Image
+        src="/favicon.ico"
+        alt="Gen Fulton website logo"
+        width={70}
+        height={70}
+      />
+      <h2>Gen Fulton</h2>
+    </Link>
+    <div className="grid grid-rows-3 grid-flow-col gap-1 max-[1306px]:pb-6">
+      {footerLinks.map((item, index) => {
+        const isHidden = index >= footerLinks.length - 2;
+        return (
+          <ul key={index} className="flex">
+            <li
+              className={`flex  items-center ${
+                isHidden ? "hidden min-[1306px]:flex" : ""
+              }`}
+            >
+              {index > 2 ? (
+                <Link
+                  href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="pl-10 max-mobileplus:pl-3"
+                >
+                  {item.name}
+                </Link>
+              ) : (
+                <Link href={item.href}>{item.name}</Link>
+              )}
+            </li>
+          </ul>
+        );
+      })}
+    </div>
+  </div>
+);
+const ContactInfo = () => (
+  <>
+    <span>Gen Fulton Consultancy</span>
+    <Link href="mailto:info@genfulton.com">
+      <div className="flex gap-2 aspect-3/4">
+        <Image
+          src="/footer/email.svg"
+          alt="Gen Fulton's email address"
+          width={24}
+          height={24}
+        />
+        <span>info@genfulton.com</span>{" "}
+      </div>
+    </Link>
+
+    <Link href="tel:+14155792187">
+      <div className="flex gap-2 aspect-3/4">
+        <Image
+          src="/footer/whatsapp.png"
+          alt="Gen Fulton's WhatsApp phone number"
+          width={24}
+          height={24}
+        />
+        <span>+1 (415) 579-218</span>
+      </div>
+    </Link>
+  </>
+);
+
+const SocialIcons = () => (
+  <div className="flex gap-4">
+    <Link
+      href="https://www.linkedin.com/in/gen-fulton-lmft-sep-96a38566/"
+      target="_blank"
+    >
+      <Image
+        src="/footer/linkedin.png"
+        alt="Gen Fulton's LinkedIn"
+        width={18}
+        height={17}
+      />
+    </Link>
+    <Link
+      href="https://www.instagram.com/genfultonconsultancy/"
+      target="_blank"
+    >
+      <Image
+        src="/footer/instagram.png"
+        alt="Gen Fulton's Instagram"
+        width={18}
+        height={17}
+      />
+    </Link>
+  </div>
+);
 
 export default Footer;
